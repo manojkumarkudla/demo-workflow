819ebc835e189aa9799ca9dc951dfdf6ec006908
Manoj Kumar <manoj.kumar@cloudreach.com>
2022-11-23 15:00:33 +0530

    latest update

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "docker_vpc" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.docker_vpc.id
   cidr_block = var.cidr_public
-  # map_public_ip_on_launch = true
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public"
